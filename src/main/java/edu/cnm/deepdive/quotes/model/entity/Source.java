70b734cfc1eb9a1e03f02310ed7c474118882fb3
Mickie Morlang <mmorlang@gmail.com>
2020-07-06 10:13:16 -0600

    many to many relationship stack overflow error

--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Source.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Source.java
@@ -1,16 +1,23 @@
 package edu.cnm.deepdive.quotes.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.lang.NonNull;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 @SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
@@ -38,6 +45,13 @@ public class Source {
   @Column(nullable = false)
   private Date updated;
 
+  @OneToMany(fetch = FetchType.LAZY,
+      mappedBy = "source",
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+
+  @OrderBy("text ASC")
+  private List<Quote> quotes = new LinkedList<>();
+
   @NonNull
   public Long getId() {
     return id;
@@ -60,5 +74,9 @@ public class Source {
 
   public Date getUpdated() {
     return updated;
+  }
+
+  public List<Quote> getQuotes() {
+    return quotes;
   }
 }
