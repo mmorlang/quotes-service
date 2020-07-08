075a841aa84806adcfa0df7033f930356bf57b8c
Mickie Morlang <mmorlang@gmail.com>
2020-07-08 16:57:22 -0600

    adding Endpoints and controllers.

--- src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,5 +66,11 @@ public class QuoteController {
   @GetMapping(value = "/{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote get(@PathVariable long id) {
     return quoteRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+  }
+
+
+  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Quote> search(@RequestParam(name = "q", required = true) String filter) {
+    return quoteRepository.getAllByTextContainingOrderByTextAsc(filter);
   }
 }
--- src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.quotes.service;
 import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
+import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
@@ -14,4 +15,8 @@ public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
 
   Iterable<Quote> getAllByTagsContainingOrderByTextAsc(Tag tag);
+
+  Iterable<Quote> getAllByTextContainingOrderByTextAsc(String filter);
+
+  Iterable<Quote> getAllByCreatedBetweenOrderByCreatedAsc(Date start, Date end);
 }
