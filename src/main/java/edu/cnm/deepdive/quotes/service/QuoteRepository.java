1b02a5ed6554e55a30fe16f068991e8548429c71
Mickie Morlang <mmorlang@gmail.com>
2020-07-13 11:24:02 -0600

    Oauth integration.

--- src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
@@ -4,19 +4,25 @@ import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
 import java.util.Date;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
   Iterable<Quote> getAllByOrderByTextAsc();
 
-
   Iterable<Quote> getAllBySourceOrderByTextAsc(Source source);
-
 
   Iterable<Quote> getAllByTagsContainingOrderByTextAsc(Tag tag);
 
   Iterable<Quote> getAllByTextContainingOrderByTextAsc(String filter);
 
-  Iterable<Quote> getAllByCreatedBetweenOrderByCreatedAsc(Date start, Date end);
+  @SuppressWarnings("SqlResolve")
+  @Query(value = "SELECT * FROM sa.Quote ORDER BY RANDOM() OFFSET 0 ROWS FETCH NEXT 1 ROW ONLY",
+      nativeQuery = true)
+  Optional<Quote> getRandom();
+
+  @Query("SELECT COUNT(q) FROM Quote AS q")
+  long getCount();
 }
